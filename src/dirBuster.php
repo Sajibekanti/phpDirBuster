5d77502904586a8b7a5044af978d71ba93dd2a93
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2019-06-03 20:28:59 +0200

    Update.
    Return the array with the result.

--- src/dirBuster.php
+++ src/dirBuster.php
@@ -2,17 +2,24 @@
 namespace MattiaBLX\phpDirBuster;
 
 class dirBuster {
+    public function __construct()
+    {
+        $this->bust = [];
+    }
+
     public function Buster($dir = __DIR__.'/') {
         foreach (scandir($dir) as $element) {
             $finaldir = $dir.$element;
             if(!in_array($element, ['.', '..'])){
                 if (is_dir($finaldir)) {
-                    echo $finaldir.PHP_EOL;
+                    $this->bust['directories'][] = $finaldir;
                     $this->Buster($finaldir.'/');
                 }else{
-                    echo $finaldir.PHP_EOL;
+                    $this->bust['files'][] = $finaldir;
                 }
+                $this->bust['total'][] = $finaldir;
             }
         }
+        return $this->bust;
     }
 }
